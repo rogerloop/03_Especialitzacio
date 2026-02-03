0c5f5835c0089490670dfb6e03e7ef382cb89e3e
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2026-02-03 20:31:18 +0100

    Level 2 finished

--- Sprint_2_DB_relacionals/S2_01_Nocions_basiques SQL/S2_01__Script_Nocions_basiques .sql
+++ Sprint_2_DB_relacionals/S2_01_Nocions_basiques SQL/S2_01__Script_Nocions_basiques .sql
@@ -144,6 +144,7 @@ ORDER BY mitjana_vendes DESC
 LIMIT 1
 ;
 
+
 /*  
 Exercici 3
 
@@ -161,11 +162,129 @@ FROM transaction t
 WHERE t.company_id IN (
     SELECT c.id
     FROM company c
-    WHERE country = 'germany'
+    WHERE country = 'Germany'
 )
 ;
 
 -- Llista les empreses que han realitzat transaccions per un amount superior a la mitjana de totes les transaccions.
 
 
-SELECT t.
+SELECT c.company_name
+FROM company c
+WHERE c.id IN (
+    SELECT DISTINCT t.company_id
+    FROM transaction t
+    WHERE t.amount > (
+        SELECT AVG (t.amount)
+        FROM transaction t
+    )
+)
+;
+
+-- Eliminaran del sistema les empreses que no tenen transaccions registrades, entrega el llistat d'aquestes empreses.
+
+-- No hi ha cap empresa que no tingui transaccions finalitzades OK el llistat estaria buit. Amb select distict trobem totes
+-- les empreses uniques que tenen transaccions (en aquest cas 100, totes)
+
+SELECT c.company_name
+FROM company c
+WHERE c.id NOT IN (
+    SELECT DISTINCT t.company_id
+    FROM transaction t
+    WHERE t.declined = 0
+)
+;
+
+
+/*  
+Nivell 2
+
+Exercici 1
+
+Identifica els cinc dies que es va generar la quantitat més gran d'ingressos a l'empresa per vendes. 
+Mostra la data de cada transacció juntament amb el total de les vendes.
+*/;
+
+SELECT  DATE (t.timestamp) AS data,
+        SUM (t.amount) vendes_diaries
+FROM transaction t
+GROUP BY DATE (t.timestamp)
+ORDER BY vendes_diaries DESC
+LIMIT 5
+;
+
+
+/*
+Exercici 2
+
+Quina és la mitjana de vendes per país? 
+Presenta els resultats ordenats de major a menor mitjà.
+*/;
+
+SELECT  c.country AS pais,
+        AVG (t.amount) AS mitjana_vendes
+FROM transaction t
+JOIN company c ON t.company_id = c.id
+GROUP BY c.country
+ORDER BY mitjana_vendes DESC
+;
+
+
+/*
+Exercici 3
+
+En la teva empresa, es planteja un nou projecte per a llançar algunes campanyes publicitàries per a fer competència a la companyia "Non Institute". 
+Per a això, et demanen la llista de totes les transaccions realitzades per empreses que estan situades en el mateix país que aquesta companyia.
+
+Mostra el llistat aplicant JOIN i subconsultes.
+Mostra el llistat aplicant solament subconsultes.
+*/;
+
+-- Mostra el llistat aplicant JOIN i subconsultes.
+
+SELECT *
+FROM company c
+JOIN transaction t ON c.id = t.company_id
+WHERE c.country = (
+    SELECT c.country
+    FROM company c
+    WHERE c.company_name = 'Non Institute'
+)
+;
+
+-- Mostra el llistat aplicant solament subconsultes.
+
+SELECT *
+FROM transaction t
+WHERE t.company_id IN (
+    SELECT c.id
+    FROM company c
+    WHERE c.country = (
+        SELECT c.country
+        FROM company c
+        WHERE c.company_name = 'Non Institute'
+    )
+)
+;
+
+
+/*
+Nivell 3
+
+Exercici 1
+
+Presenta el nom, telèfon, país, data i amount, d'aquelles empreses que van realitzar transaccions amb un valor comprès entre 350 i 400 euros
+ i en alguna d'aquestes dates: 29 d'abril del 2015, 20 de juliol del 2018 i 13 de març del 2024. Ordena els resultats de major a menor quantitat.
+*/
+
+
+
+
+/*
+Exercici 2
+
+Necessitem optimitzar l'assignació dels recursos i dependrà de la capacitat operativa que es requereixi, 
+per la qual cosa et demanen la informació sobre la quantitat de transaccions que realitzen les empreses, 
+però el departament de recursos humans és exigent i vol un llistat de les empreses on especifiquis si tenen més de 400 transaccions o menys.
+
+*/
