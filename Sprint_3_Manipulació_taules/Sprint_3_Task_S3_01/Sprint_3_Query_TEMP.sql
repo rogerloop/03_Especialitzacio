2ab5cff59c4cd0ae7dce574a999d7b2797101789
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2026-02-12 19:28:33 +0100

    Create Sprint_3_Query_TEMP.sql

---
+++ Sprint_3_Manipulació_taules/Sprint_3_Task_S3_01/Sprint_3_Query_TEMP.sql
@@ -0,0 +1,50 @@
+/*
+Nivell 3
+Exercici 1
+La setmana vinent tindràs una nova reunió amb els gerents de màrqueting. 
+Un company del teu equip va realitzar modificacions en la base de dades, 
+però no recorda com les va realitzar.
+ Et demana que l'ajudis a deixar els comandos executats per a obtenir el següent diagrama:
+
+ Recordatori
+
+En aquesta activitat, és necessari que descriguis el "pas a pas" de les tasques realitzades. 
+És important realitzar descripcions senzilles, simples i fàcils de comprendre. 
+Per a realitzar aquesta activitat hauràs de treballar amb els arxius denominats
+ "estructura_dades_user" i "dades_introduir_user"
+
+Recorda continuar treballant sobre el model i les taules amb les quals ja has treballat fins ara.
+*/
+
+/*
+Step 2: Rename table 'user' —> 'data_user'
+*/
+ALTER TABLE `user`
+RENAME TO data_user
+;
+
+/*
+Step 3: Change column type 'id' from table 'data_user' to INT
+*/
+ALTER TABLE data_user
+MODIFY COLUMN id INT
+;
+/*
+Step 4: Change column name 'email' from table 'data_user' to ‘personal_email’
+*/
+ALTER TABLE data_user
+RENAME COLUMN email TO personal_email
+;
+
+/*Step 5: Create de FOREIGN KEY constraint between 
+table ‘transaction’ and ‘data_user’
+*/
+
+ALTER TABLE transaction
+ADD CONSTRAINT FK_transaction_datauser
+FOREIGN KEY (user_id) REFERENCES data_user(id)
+;
+
+INSERT INTO `user` (id)
+VALUES (9999)
+ON DUPLICATE KEY UPDATE id = id;
