a482aaacdca150e7dbf3d5c4578b46a6163e095e
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2026-02-18 12:32:07 +0100

    Update Sprint_4_Modelat SQL.sql

--- Sprint_4_Modelat_SQL/Sprint_4_Modelat SQL.sql
+++ Sprint_4_Modelat_SQL/Sprint_4_Modelat SQL.sql
@@ -11,3 +11,33 @@ CHARACTER SET utf8mb4;	-- Character Definition for Enhanced Compatibility with â
 
 USE starmarketplace
 ;
+
+-- Table creation 
+
+-- Creation table 'products'
+CREATE TABLE  products (
+	id INT,
+    product_name VARCHAR(255),
+    price VARCHAR(255),
+    colour VARCHAR(50),
+    wheight VARCHAR(50),
+    warehouse_id VARCHAR(50)
+);
+
+-- Comando para evitar restricciones de MySQL en MacOS
+SET GLOBAL local_infile = 1;
+
+-- Importar datos de CSV a products
+LOAD DATA LOCAL
+INFILE '/Users/rogerdefez/Documents/Cursos i Llibres/BootCamp IT Academy/03_Especialitzacio/Sprint_4_Modelat_SQL/Dades_originals_S4/products.csv'
+INTO TABLE products
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+IGNORE 1 ROWS
+;
+
+
+
+SHOW VARIABLES LIKE 'local_infile';
+
+
