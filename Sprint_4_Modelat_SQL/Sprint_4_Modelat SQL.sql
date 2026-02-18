14edb2bf036af94e6062615059921611214bbda3
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2026-02-18 01:01:42 +0100

    Update Sprint_4_Modelat SQL.sql

--- Sprint_4_Modelat_SQL/Sprint_4_Modelat SQL.sql
+++ Sprint_4_Modelat_SQL/Sprint_4_Modelat SQL.sql
@@ -1,1 +1,13 @@
-CREATE SCHEMA `starmarketplace` ;
+/*
+Level 1
+Descàrrega els arxius CSV, 
+estudia'ls i dissenya una base de dades amb un esquema d'estrella que contingui, 
+almenys 4 taules de les quals puguis realitzar les següents consultes:
+*/
+
+-- Database creation and inicialization
+CREATE DATABASE starmarketplace 
+CHARACTER SET utf8mb4;	-- Character Definition for Enhanced Compatibility with “ñ” and Other Characters in MySQL 8
+
+USE starmarketplace
+;
