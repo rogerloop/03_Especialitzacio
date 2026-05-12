a9d15da0574976b81a3e7a872b1b14fb7a7cc836
GitHub Desktop RGR <bascular-rendiment0x@icloud.com>
2026-05-12 13:37:10 +0200

    update

--- Pildora_Streamlit/Streamlit.py
+++ Pildora_Streamlit/Streamlit.py
@@ -5,4 +5,6 @@ st.title("Hello world! Buen trabajo Alana")
 
 
 # streamlit run Streamlit.py   Para correr streamlit en un servidor local
+# Si público y con librerias, hay que tener un requirements txt en el directorio raiz del proyecto o repositorio
+
 
